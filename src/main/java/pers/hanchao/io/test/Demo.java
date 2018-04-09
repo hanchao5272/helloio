24c241a9ea6866c23e6b6f6780bf75471a883094
hanchao5272 <89298141@qq.com>
2018-04-09 23:09:41 +0800

    log4j;README.md;.gitignore

--- src/main/java/pers/hanchao/io/test/Demo.java
+++ src/main/java/pers/hanchao/io/test/Demo.java
@@ -1,11 +1,15 @@
 package pers.hanchao.io.test;
 
+import org.apache.log4j.Logger;
+
 public class Demo {
+    private static final Logger LOGGER = Logger.getLogger(Demo.class);
     /**
      * <p></p>
      * @author hanchao 2018/4/9 22:59
      **/
     public static void main(String[] args) {
          System.out.println(1111);
+         LOGGER.info(1111);
     }
 }
