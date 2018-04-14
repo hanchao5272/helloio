a1e619abb3010b4e8912adc484043dad20542a27
hanchao5272 <89298141@qq.com>
2018-04-14 23:49:19 +0800

    IO-02:文件字节流的wirte和read方法
    1.fos.write(byte):向文件中写一个byte
    2.fos.write(bytes):向文件中写一个byte数组
    3.fos.write(bytes,off,len):向文件中写入指定byte数组的部分数据
    4.fis.available():剩余可读byte数量
    5.fis.read():读取一个字节
    6.fis.read(bytes)：读取bytes.length数组长度的字节并将读取结果放在bytes中
    7.fis.read(bytes,off,len)：读取len长度的字节，并将其在bytes数组的第off个元素开始存放

--- src/main/java/pers/hanchao/io/file/FileIODemo.java
+++ src/main/java/pers/hanchao/io/file/FileIODemo.java
@@ -30,8 +30,10 @@ public class FileIODemo {
         3 FileWriter与FileReader             字符流的范围
         4 FileWriter与FileReader             字符流与字符流
         5 FileWriter与FileInputStream        字符流与字节流
+        11 FileOutputStream与FileInputStream FileOutputStream的写方法和FileInputStream的读方法
+        12 FileWriter与FileReader            FileWriter的写方法与FileReader的读方法
          */
-        int type = 5;
+        int type = 11;
         switch (type) {
             case 0:
                 System.out.println("字节流处理的是字节流(byte)，数值范围：0~255");
@@ -59,6 +61,7 @@ public class FileIODemo {
                 //以字节流的方式读取文件
                 int i = 0;
                 char c;
+                //通过读的返回值是否为-1判断是否还有值
                 while ((i = fis.read()) != -1) {
                     //字节转换成字符
                     c = (char) i;
@@ -124,6 +127,50 @@ public class FileIODemo {
                 fileWriter.flush();
                 System.out.println("49 ==> " + fis.read());
                 System.out.println("20013 ==> " + fis.read());
+                break;
+            case 11:
+                //FileOutputStream的基本方法
+                //fos.write(byte):向文件中写一个byte
+                int a = 66;
+                fos.write(a);
+                //fos.write(bytes):向文件中写一个byte数组
+                byte[] bytes2 = new byte[]{49, 50, 51, 65, 66, 67};
+                fos.write(bytes2);
+                //fos.write(bytes,off,len):向文件中写入指定byte数组的部分数据
+                fos.write(bytes2,1,2);
+                //将缓冲区的数据强制输出
+                fos.flush();
+                //fis.available():剩余可读byte数量
+                if (fis.available() > 0){
+                    System.out.println("剩余可读byte数量：" + fis.available());
+                    //fis.read()读取一个字节
+                    System.out.println("\nfis.read()读取一个字节: " +  fis.read());
+                    System.out.println("剩余可读byte数量：" + fis.available());
+                    //fis.read(bytes)：读取bytes.length数组长度的字节并将读取结果放在bytes中
+                    byte[] bb = new byte[2];
+                    int length1 = fis.read(bb);
+                    System.out.println("\n读取了" + length1 + "个字节");
+                    for (int j = 0; j < bb.length; j++) {
+                        System.out.println(bb[j] + " ==> " + (char)bb[j]);
+                    }
+                    System.out.println("剩余可读byte数量：" + fis.available());
+                    //fis.read(bytes,off,len)：读取len长度的字节，并将其在bytes数组的第off个元素开始存放锁
+                    byte[] bbb = new byte[3];
+                    int length2 = fis.read(bbb,1,1);
+                    System.out.println("\n读取了" + length2 + "个字节");
+                    for (int k = 0; k < bbb.length; k++) {
+                        System.out.println(bbb[k] + " ==> " + (char)bbb[k]);
+                    }
+                    System.out.println("剩余可读byte数量：" + fis.available());
+                    //如果没有那么多字符，则读取可读的数量
+                    byte[] aa = new byte[6];
+                    int length3 = fis.read(aa);
+                    System.out.println("\n读取了" + length3 + "个字节");
+                    for (int j = 0; j < aa.length; j++) {
+                        System.out.println(aa[j] + " ==> " + (char)aa[j]);
+                    }
+                    System.out.println("剩余可读byte数量：" + fis.available());
+                }
                 break;
             default:
                 break;
